2b5b40a149be2ac3e996647c4316ae45cc2b96f2
ytang-eng <ytang6442@gmail.com>
2026-04-27 12:43:25 -0400

    Refactor DecryptionTest by removing comments
    Removed comments and ensured proper exception message verification.

--- src/test/java/jpass/crypt/DecryptionTest.java
+++ src/test/java/jpass/crypt/DecryptionTest.java
@@ -7,10 +7,8 @@ public class DecryptionTest {
 
     @Test
     public void testExceptionInstantiation() {
-        // White-box: Instantiate the exception to achieve 100% class/line coverage
+        
         DecryptException exception = new DecryptException();
-
-        // Verify the hardcoded message defined in the default constructor
         assertEquals("Decryption failed.", exception.getMessage(),
                 "The exception message should match the hardcoded default string.");
     }
